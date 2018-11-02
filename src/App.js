14b48fbbaad1745005c86fb430fa3ce95080eba8
William Lim <weinianlim26@gmail.com>
2018-11-02 00:08:46 -0700

    Add Nav Bar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>William Lim's Life</h1>
-        <h2>Life be awesome with Alexis</h2>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">William Logo</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
