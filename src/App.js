51a9b96a3ff264789efe159c8fc00ceac2ab53c9
WeiNianLim <weinianlim26@gmail.com>
2018-11-25 22:32:20 -0800

    Bootstrap to Material ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
 class App extends Component {
+
+  state = {
+    open: false
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open : true})
+  }
+
+  handleDrawerClose = () => {
+    this.setState({ open : false})
+  }
+
   render() {
     return (
       <div className="App">
         <div id="landingImg">
-          <nav className="navbar navbar-expand-lg navbar-inverse fixed-top1 transparent ">
-            <a className="navbar-brand" href="#">William Logo</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse navbar-inner" id="navbarText">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Projects</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Contact Me</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <div id="layer">
+            <AppBar position="absolute">
+              <Toolbar>
+                <IconButton onClick={this.handleDrawerOpen}>
+                  <MenuIcon/>
+                </IconButton>
+              </Toolbar>
+            </AppBar>
+            <Drawer open={this.state.open} onClose={this.handleDrawerClose}>
+              <List>
+                <ListItem>
+                  About
+                </ListItem>
+                <ListItem>
+                  Projects
+                </ListItem>
+                <ListItem>
+                  Contact Me
+                </ListItem>
+              </List>
+            </Drawer>
+          </div>
         </div>
       </div>
     );
