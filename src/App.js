41a52e3c70a652a86637912978b7b73dfe062576
willPortonHealth <Devback@portonhealth.ca>
2018-12-02 21:22:21 -0800

    ad withWidth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -13,6 +14,20 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
+import Grid from '@material-ui/core/Grid';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+
+const styles = theme => ({
+  appBar: {
+    backgroundColor: "transparent",
+    boxShadow: "none"
+  },
+
+  iconButton: {
+    color: "white"
+  }
+})
 
 class App extends Component {
 
@@ -29,17 +44,30 @@ class App extends Component {
   }
 
   render() {
+    const {classes, width} = this.props
     return (
       <div className="App">
         <div id="landingImg">
           <div id="layer">
-            <AppBar position="absolute">
-              <Toolbar>
-                <IconButton onClick={this.handleDrawerOpen}>
-                  <MenuIcon/>
-                </IconButton>
-              </Toolbar>
-            </AppBar>
+            
+              {
+                width === 'md' || width === 'lg' || width === 'xl'
+                ? <Grid container spacing={32} justify="space-evenly" style={{margin: "0"}}>
+                    <Grid item style={{fontWeight: "900"}}>About</Grid>
+                    <Grid item style={{fontWeight: "900"}}>Projects</Grid>
+                    <Grid item style={{padding: "0", color: "white"}}><a href="#"><img src="/images/logo.png" style={{maxHeight : "100%", maxWidth: "100%"}}/></a></Grid>
+                    <Grid item style={{fontWeight: "900"}}>Blog</Grid>
+                    <Grid item style={{fontWeight: "900"}}>Contact Me</Grid>
+                  </Grid>
+                : <AppBar position="absolute" className={classes.appBar}>
+                    <Toolbar>
+                      <IconButton onClick={this.handleDrawerOpen} className={classes.iconButton}>
+                        <MenuIcon/>
+                      </IconButton>
+                    </Toolbar>
+                  </AppBar>
+              }
+            
             <Drawer open={this.state.open} onClose={this.handleDrawerClose}>
               <List>
                 <ListItem>
@@ -54,10 +82,15 @@ class App extends Component {
               </List>
             </Drawer>
           </div>
+         
         </div>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withWidth()(withStyles(styles)(App));
