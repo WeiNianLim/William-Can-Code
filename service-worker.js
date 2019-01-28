e8fd6efaa712f856b2a04ee93f4b8ac1c8330eb3
William <weinianlim26@gmail.com>
2019-01-27 17:33:46 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/William-Can-Code/precache-manifest.5aa3051c34b360e89f898476644d48c2.js"
+  "/William-Can-Code/precache-manifest.d02d08f380100e8cf07444c9538cb02b.js"
 );
 
 workbox.clientsClaim();
